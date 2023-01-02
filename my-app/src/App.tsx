1185790cb291e6c83187afab6babca6659a15473
Webster <strategywebstermanyanga@gmail.com>
2023-01-02 13:36:54 +0100

    created TopNav component

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
+import { TopNav } from './components/TopNav/TopNav';
 
 function App() {
   return (
     <div className="App">
       <header className="header">
-        <nav className="top-nav">
-          <div className="logo"></div>
-          <div className="deliverTo">
-            Deliver to: 
-            <span className="deliverTo__current">
-              Current Location Warsaw
-            </span>
-          </div>
-          <div className="top-nav__searchLogin">
-            <input type="search" placeholder='Search Food' />
-            <button>Login</button>
-          </div>
-        </nav>
+        <TopNav />
       </header>
     </div>
   );
