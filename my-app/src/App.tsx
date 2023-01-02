0362b6783b7fb0cf7692c859f4a4bea210bbb6f2
Webster <strategywebstermanyanga@gmail.com>
2023-01-02 12:41:48 +0100

    top navigation html

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,6 +3,21 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
+      <header className="header">
+        <nav className="top-nav">
+          <div className="logo"></div>
+          <div className="deliverTo">
+            Deliver to: 
+            <span className="deliverTo__current">
+              Current Location Warsaw
+            </span>
+          </div>
+          <div className="top-nav__searchLogin">
+            <input type="search" placeholder='Search Food' />
+            <button>Login</button>
+          </div>
+        </nav>
+      </header>
     </div>
   );
 }
